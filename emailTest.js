d3734e0e81847c7106b9245abc07befb9c302c6b
Mark Morrison <mmorrisonlk@gmail.com>
2023-05-09 13:05:45 -0500

    Email Test Success

---
+++ emailTest.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer');
+require('dotenv').config();
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+var mailOptions = {
+  from: process.env.EMAIL_USER,
+  to: process.env.RECIPIENT,
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,7 @@
 var http = require('http');
+require('dotenv').config();
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end(process.env.INFINITE_SECRETS);
 }).listen(8080);
